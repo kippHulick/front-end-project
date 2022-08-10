6a1173ed7889604f141182dbd8b2ee05341ed81b
Austin Desana <austindesana@outlook.com>
2022-08-10 16:16:12 -0500

    modal is working on punchParty

--- javascript/rum.js
+++ javascript/rum.js
@@ -5,8 +5,8 @@ let info = document.querySelector('.md')
 console.log(modal.href)
 htmlStr = ''
 modal.addEventListener('click', function(){
-  console.log(modal.name)
-  
+  let name = document.getElementsByName(name)
+  console.log(name)
   htmlStr += `    <div class="modal-container" id="${modal.name}">
   <div class="modal">
     <h1 class="modal__title">Modal 1 Title</h1>
