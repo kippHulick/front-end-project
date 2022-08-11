4ba063c78fdb8fd82b28ef53fd6de047791eff8d
Tommy Zimmerman <tomzimmerman7@protonmail.com>
2022-08-11 17:39:02 -0400

    fixed global modal (finally)

--- javascript/Cocoa2.js
+++ javascript/Cocoa2.js
@@ -33,32 +33,32 @@ const ordDrink = async () => {
         htmlStr += `<a class="card" href="#${idDrink}" id="${strDrink}" title="${strDrink}">
         <div class="card__background" id="card" style="background-image: url(${strDrinkThumb})" title="${strDrink}"></div>
         <div class="card__content" id="card" title="${strDrink}">
-          <p class="card__category" id="card" title="${strDrink}">${strCategory}</p>
-          <h3 class="card__heading" id="card" title="${strDrink}">${strDrink}</h3>
+        <p class="card__category" id="card" title="${strDrink}">${strCategory}</p>
+        <h3 class="card__heading" id="card" title="${strDrink}">${strDrink}</h3>
         </div>
-      </a>
-  `
-            let newDrink = new DrinkInfo(strDrink, idDrink, strCategory)
-
-            drinkInfoArr.push(newDrink)
-
-        }
-
-
-    })
-    document.addEventListener('click', e=>{
-
-        if(e.target.id == 'card'){
-
-            // let targetName = e.target.title
-
-            // for(let i = 0; i < drinkInfoArr.length; i++){
-
+        </a>
+        `
+        let newDrink = new DrinkInfo(strDrink, idDrink, strCategory)
+        
+        drinkInfoArr.push(newDrink)
+        
+    }
+    
+    
+})
+document.addEventListener('click', e=>{
+    
+    if(e.target.id == 'card'){
+        
+        // let targetName = e.target.title
+        
+        // for(let i = 0; i < drinkInfoArr.length; i++){
+            
             //     if(drinkInfoArr[i].name == targetName){
-
-            //         modal.innerHTML = `      
-                    
-            //         <div class="modal-container" id="${drinkInfoArr[i].id}">
+                
+                //         modal.innerHTML = `      
+                
+                //         <div class="modal-container" id="${drinkInfoArr[i].id}">
             //         <div class="modal">
             //           <h1 class="modal__title">${drinkInfoArr[i].name}</h1>
             //           <p class="modal__text"> ${drinkInfoArr[i].category}</p>
@@ -70,26 +70,25 @@ const ordDrink = async () => {
             //         </div>`
             //     }
             // }
-
+            
             appearModal('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007').then((result)=>{
-
-
+                
+                
                 console.log(result);
                 modal.innerHTML = result
             })
-
+            
             document.querySelector('.md').style.display = 'flex';
         }
-
-       
+        
+        
         if(e.target.id == 'x'){
             
             modal.style.display = 'none';
         }
     })
-
-
-
+    
+    
+    
     cardgrid.innerHTML = htmlStr
-  })
-  
+})
