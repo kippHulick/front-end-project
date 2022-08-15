e0dedfedd7b3c03a83a6abfed572f52cc8966d6b
Tommy Zimmerman <tomzimmerman7@protonmail.com>
2022-08-15 11:24:37 -0400

    better search by ingredient

--- javascript/searchByIngredient.js
+++ javascript/searchByIngredient.js
@@ -1,129 +1,44 @@
-class DrinkInfo{
-    constructor(name, ingredients){
-        
-        this.name = name
-        this.ingredients = ingredients
-        
-    }
-    
-}
 
 const drinkArr = []
 const modal = document.querySelector('.md')
 let input = document.querySelector('input')
 
+document.addEventListener('submit', (e)=>{
 
-const getDrinks = async () => {
-    
-    // From this line to 232, alls that is being done is arrays are being made in order to fetch all of the avalible drinks from the api, a straightforward way to get every avalible drink was the API's 'list all cokctails by first letter', which was why I opted to loop through the alphabet. 
-    let drinkArr = [];
-    let resultsArr = [];
-    let drinkInfoArr = [];
-    let alphabet = '1234567890abcdefghijklmnopqrstuvwxyz';
-    
-    
-    for(let i = 0; i < alphabet.length; i++){
-        
-        let drinks = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${alphabet[i]}`);
-        resultsArr = await drinks.json();
-        
-        drinkArr.push(resultsArr)
-    }
-    
-    console.log(drinkArr);
-    
-    for(let i = 0; i < drinkArr.length; i++){
-        
-        try{
-            
-            for(let j = 0; j < drinkArr[i].drinks.length; j++){
-                
-                
-                let drinks = drinkArr[i].drinks[j]
-                let ingArr = []
-                
-                for(let x = 1; x < 16; x++){
-                    
-                    ingredient = `strIngredient${x}`
-                    
-                    if(drinks[ingredient] != null){
-                        
-                        let ingredientInLower = drinks[ingredient].toLowerCase()
-                        ingArr.push(ingredientInLower)
-                    }
-                }
-                
-                let newDrink = new DrinkInfo(drinks.strDrink, ingArr)
-                
-                drinkInfoArr.push(newDrink)
-            }
-            
-        }
-        
-        catch(error){
-            
-            console.log(error);
-        }
-    }
-    
-    document.addEventListener('submit', (e)=>{
-        
-        const randomDrinks = () => {
-            
-            e.preventDefault()
-            let drinkSearch = (input.value.toLowerCase())
-            let promArr = []
-            
-            
-            for(let i = 0; i < drinkInfoArr.length; i++){
-                
-                for(let j = 0; j < drinkInfoArr[i].ingredients.length; j++){
-                    
-                    if(drinkInfoArr[i].ingredients[j] == drinkSearch){
-                        let randDrink =  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkInfoArr[i].name}`).then(resp => resp.json())
-                        promArr.push(randDrink)
-                    }
-                }
-            }
-            //   let randDrink =  fetch(`https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=${drinkSearch}`).then(resp => resp.json())
-            //   promArr.push(randDrink)
-            return Promise.all(promArr)
-        }
-        
-        // try{
-            
-            randomDrinks().then(data => {
-                let htmlStr = ''
-                console.log(data);
-                data.forEach(drinkObj => {
-                    for(let i = 0; i < drinkObj.drinks.length; i++){
-                        let drink = drinkObj.drinks[i]
-                        drinkArr.push(drink)
-                        let { strDrink, strDrinkThumb, strCategory, idDrink } = drink
-                        htmlStr += 
-              `<div class="card" id="${idDrink}" title="card" href="#">
-              <div class="card__background" style="background-image: url(${strDrinkThumb})" id="${idDrink}" title="card"></div>
-              <div class="card__content" id="${idDrink}" title="card">
-              <p class="card__category" id="${idDrink}" title="card">${strCategory}</p>
-              <h3 class="card__heading" id="${idDrink}" title="card">${strDrink}</h3>
-              </div>
-              </div>`
+  const randomDrinks = () => {
+    e.preventDefault()
+    let drinkSearch = (input.value)
+    let promArr = []
+    let randDrink =  fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${drinkSearch}`).then(resp => resp.json())
+    promArr.push(randDrink)
+    return Promise.all(promArr)
+}
+
+
+    randomDrinks().then(data => {
+        let htmlStr = ''
+        console.log(data);
+        data.forEach(drinkObj => {
+            for(let i = 0; i < drinkObj.drinks.length; i++){
+            let drink = drinkObj.drinks[i]
+            drinkArr.push(drink)
+            let { strDrink, strDrinkThumb, strCategory, idDrink } = drink
+            htmlStr += 
+        `<div class="card" id="${idDrink}" title="card" href="#">
+            <div class="card__background" style="background-image: url(${strDrinkThumb})" id="${idDrink}" title="card"></div>
+                <div class="card__content" id="${idDrink}" title="card">
+                    <p class="card__category" id="${idDrink}" title="card">${strCategory}</p>
+                    <h3 class="card__heading" id="${idDrink}" title="card">${strDrink}</h3>
+                </div>
+        </div>`
             }
         })
         
         document.querySelector('.card-grid').innerHTML = htmlStr
     })
-    //}
-    // catch{
-        //     console.log(error);
-        // }
-        
-    })
-    
-}
 
-getDrinks()
 
+})
 
 document.addEventListener('click', e => {
         
@@ -145,3 +60,4 @@ document.addEventListener('click', e => {
     }
 
 })
+
